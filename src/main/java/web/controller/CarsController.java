54e7c202bdf87222710aab9da4985f0c1d158757
Magomed Zulpukarov <zulpukarovmagomed00@gmail.com>
2024-09-24 22:27:49 +0300

    Сделал задачку

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -1,18 +1,22 @@
 package web.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
-import web.service.CarServiceImpl;
 
 @Controller
+@RequiredArgsConstructor
 public class CarsController {
-    CarService carService = new CarServiceImpl();
+
+    private final CarService carService;
+
 
     @GetMapping(value = "/cars")
-    public String printCars(@RequestParam(value = "count", required = false) Integer count, ModelMap model) {
+    public String printCars(@RequestParam(defaultValue = "10000") int count, ModelMap model) {
 
         model.addAttribute("cars", carService.getCars(count));
         return "cars";
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,29 +1,42 @@
 package web.service;
 
+import org.springframework.stereotype.Service;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class CarServiceImpl implements CarService {
 
-    List<Car> cars = List.of(new Car("Lada", "white", 2000),
+    private final List<Car> cars = List.of(new Car("Lada", "white", 2000),
             new Car("Priora", "neon violet", 2000),
             new Car("Matiz", "pink", 1000),
             new Car("R8", "carbon black", 1500),
             new Car("Camry", "white", 2500));
 
 
+//    @Override
+//    public List<Car> getCars(Integer count) {
+//        List<Car> carsCount = new ArrayList<>();
+//        if (count != null) {
+//            for (int i = 0; i < count && i < cars.size(); i++) {
+//                carsCount.add(cars.get(i));
+//            }
+//            return carsCount;
+//        } else {
+//            return cars;
+//        }
+//    }
+
     @Override
     public List<Car> getCars(Integer count) {
-        List<Car> carsCount = new ArrayList<>();
-        if (count != null) {
-            for (int i = 0; i < count && i < cars.size(); i++) {
-                carsCount.add(cars.get(i));
-            }
-            return carsCount;
-        } else {
-            return cars;
+        try {
+            return cars.stream().limit(count).toList();
+        } catch (IllegalArgumentException e) {
+            return new ArrayList<>();
         }
+
     }
 }
+
